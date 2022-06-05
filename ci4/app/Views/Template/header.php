37ddf5d14e1f942e8191a27dc3262e6627ee65f4
Faza Ardan Kusuma <fazkus47@gmail.com>
2022-06-05 16:34:27 +0700

    membuat layout web

---
+++ ci4/app/Views/Template/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title; ?></title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+    <style rel="stylesheet">
+    * {
+    margin: 0;
+    padding: 0;
+    }
+    body {
+        line-height:1;
+        font-size:100%;
+        font-family:'Open Sans', sans-serif;
+        color:#5a5a5a;
+    }
+    /* header */
+    header {
+        padding: 20px;
+    }
+    header h1 {
+        margin: 20px 10px;
+        color: #b5b5b5;
+    }
+    </style>
+    
+</head>
+<body>
+
+<div class="container" style="box-shadow: 0 0 1em #cccccc">
+    <div class="row">
+        <header>
+            <h1>Layout Sederhana</h1>
+        </header>
+    </div>
+    <div class="row">
+        <nav class="navbar navbar-collapse navbar-expand navbar-dark bg-primary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav" style="font-size: 14px; font-weight: bold;">
+                        <li class="nav-item" style="padding-right: 15px;">
+                            <a class="nav-link active" href="/">Home</a>
+                        </li>
+                        <li class="nav-item" style="padding-right: 15px;">
+                            <a class="nav-item">
+                                <a href="artikel" class="nav-link">Artikel</a>
+                            </a>
+                        </li>
+                        <li class="nav-item" style="padding-right: 15px;">
+                            <a class="nav-item">
+                                <a href="about" class="nav-link">About</a>
+                            </a>
+                        </li>
+                        <li class="nav-item" style="padding-right: 15px;">
+                            <a class="nav-item">
+                                <a href="contact" class="nav-link">Contact</a>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div> <br>
+    <div class="row">
